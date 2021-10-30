7f43561db433cbe8e5d329d67e7add27d4e278d4
Antoine Honoré <honore@kth.se>
2021-10-30 10:53:54 +0200

    start patient display

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import plotly.graph_objects as go # or plotly.express as px
-fig = go.Figure() # or any Plotly Express function e.g. px.bar(...)
+import plotly.graph_objects as go  # or plotly.express as px
+
+fig = go.Figure()  # or any Plotly Express function e.g. px.bar(...)
 
 import dash
 from dash import Dash, dcc, html, Input, Output, State, callback
@@ -12,10 +13,16 @@ import dash_bootstrap_components as dbc
 from dash.exceptions import PreventUpdate
 from datetime import datetime
 
+all_data_tables = ["overview", "takecare", "med", "vatska", "vikt", "respirator", "pressure", "lab", "fio2", "monitorlf", "monitorhf"]
+
+
+def pat_data_q(tbl_name, ids__uid, col="*"):
+    return "select {} from {} where ids__uid=\'{}\'".format(col, tbl_name, ids__uid)
+
 
 def get_db_size():
     with engine.connect() as con:
-        d = pd.read_sql("select pg_size_pretty(pg_database_size(\'patdb\'))",con)
+        d = pd.read_sql("select pg_size_pretty(pg_database_size(\'patdb\'))", con)
     return d.loc[0, "pg_size_pretty"]
 
 
@@ -23,43 +30,45 @@ def gentbl(df):
     return dt.DataTable(id='table',
                         columns=[{"name": i, "id": i} for i in df.columns],
                         data=df.to_dict('records'),
-                         **style_tbl, page_size=10, style_table={'overflowX': 'auto'})
+                        **style_tbl, page_size=10, style_table={'overflowX': 'auto'})
 
 
-def get_update_status(e):
-    return "{} ({} ms)".format(gdate(), round(e*1000, 1))
-
+def get_update_status(start_):
+    return [gdate(),html.Br(),"({} ms)".format(round(((datetime.now() - start_).total_seconds()) * 1000, 1))]
 
 
 def cond_from_checklist(value):
-    return " and ".join([v+"=1" for v in value])
+    return " and ".join([v + "=1" for v in value])
 
+
+def get_categories():
+    with engine.connect() as con:
+        l = pd.read_sql(
+            "SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = 'view__uid_has'",
+            con)["column_name"].values.tolist()
+    return [{"label": k, "value": k} for k in [ll for ll in l if ll != "ids__uid"]]
+
+
+def new_checklist(i, init_val=None):
+    return dcc.Checklist(
+        options=get_categories(),
+        value=init_val,
+        labelStyle={"display": "inline-block"},
+        id="checklist-{}".format(i)
+    )
 
 
 dbcfg = get_dbcfg("cfg/db.cfg")
 
 # Overwrting some CSS default styles
-style_tbl=dict(
+style_tbl = dict(
     filter_action="native",
     sort_action="native",
     sort_mode="multi",
-
-style_data = {
-                 'color': 'black',
-                 'backgroundColor': 'white'
-             },
-style_data_conditional = [
-                             {
-                                 'if': {'row_index': 'odd'},
-                                 'backgroundColor': 'rgb(220, 220, 220)',
-                             }
-                         ],
-style_header = {
-    'backgroundColor': 'rgb(210, 210, 210)',
-    'color': 'black',
-    'fontWeight': 'bold'
-})
-
+    style_data={'color': 'black', 'backgroundColor': 'white'},
+    style_data_conditional=[{'if': {'row_index': 'odd'}, 'backgroundColor': 'rgb(220, 220, 220)', }],
+    style_header={'backgroundColor': 'rgb(210, 210, 210)', 'color': 'black', 'fontWeight': 'bold'}
+)
 
 engine = get_engine(verbose=False, **dbcfg)
 
@@ -68,35 +77,39 @@ app = dash.Dash(
     external_stylesheets=["https://codepen.io/chriddyp/pen/bWLwgP.css"])
 
 nav_bar_style = {"background-repeat": "no-repeat",
-     "background-position": "right top",
-     "background-size": "300px 30px",
-     "height":"5%",
-     "position":"fixed",
-     "top":"0", "width": "100%"}
+                 "background-position": "right top",
+                 "background-size": "300px 30px",
+                 "height": "5%",
+                 "position": "fixed",
+                 "top": "0", "width": "100%"}
 
 nav_bar_style = {}
 
-
 separator = html.Img(src=app.get_asset_url('line.png'), style={"width": "100%", "height": "5px"})
 
+def get_latest_update(id="update-status"):
+   return html.Div([html.P("Latest update: "), html.P("-", id=id)], className="two columns")
+
+
 navbar = html.Div(children=[html.Ul(children=[html.Div([
-                    separator,
-                    html.H1("- Oh My DB ! -", style={'text-align': 'center'}),
-                    ]),
-                html.Div([
-                    html.Button('Refresh', id='refresh-button'),
-                    ], className="two columns"),
-                html.Div([
-                    html.P("Latest update: "),
-                    html.P("-", id="update-status")
-                ], className="two columns"),
-                html.Div([
-                    html.P("Database size: "),
-                    html.P("-", id="dbinfo")
-                    ]),
-                separator
-                ])],
+    html.H1("- Oh My DB ! -", style={'text-align': 'center'}),
+]),
+    html.Div([
+        html.Button('Refresh', id='refresh-button'),
+    ], className="two columns"),
+    get_latest_update(id="update-status"),
+    html.Div([
+        html.P("Database size: "),
+        html.P("-", id="dbinfo")
+    ]),
+])],
     style=nav_bar_style)
+
+
+def execquerey(s, engine, col=None):
+    with engine.connect() as con:
+        df = pd.read_sql(s, con)
+    return df
 
 @app.callback(
     Output(component_id='body-div', component_property='children'),
@@ -110,26 +123,16 @@ def update_output(n_clicks):
     if n_clicks is None:
         raise PreventUpdate
     else:
-        with engine.connect() as con:
-            df = pd.read_sql("select * from overview;", con)
+        df = execquerey("select * from overview;", engine)
         out = gentbl(df)
-
-        elapsed = (datetime.now() - start_).total_seconds()
-
-        return out, get_update_status(elapsed), get_db_size()
-
-
-def get_categories():
-    with engine.connect() as con:
-        l = pd.read_sql("SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = 'view__uid_has'", con)["column_name"].values.tolist()
-    return [{"label": k, "value": k} for k in [ll for ll in l if ll != "ids__uid"]]
+    return out, get_update_status(start_), get_db_size()
 
 
 @app.callback(
-     Output(component_property="children", component_id="div-checklists"),
-     Input(component_id='lesschecklist-button', component_property='n_clicks'),
-     Input(component_id='morechecklist-button', component_property='n_clicks'),
-     Input(component_property="children", component_id="div-checklists"),
+    Output(component_property="children", component_id="div-checklists"),
+    Input(component_id='lesschecklist-button', component_property='n_clicks'),
+    Input(component_id='morechecklist-button', component_property='n_clicks'),
+    Input(component_property="children", component_id="div-checklists"),
 )
 def update_check_lists(clickless, clickmore, checklist):
     ctx = dash.callback_context
@@ -143,40 +146,28 @@ def update_check_lists(clickless, clickmore, checklist):
     if button_id == "lesschecklist-button":
         return checklist[:-1]
     else:
-        #print(button_id, clickless, clickmore, checklist[-1] if len(checklist) else checklist)
+        # print(button_id, clickless, clickmore, checklist[-1] if len(checklist) else checklist)
 
         if len(checklist) > 0:
             init_val = checklist[-1]["props"]["value"]
             pass
-        return checklist + [new_checklist(len(checklist)+1, init_val=init_val)]
-
-#     print(clickless, clickmore)
-#
-#     if (clickmore is None) and (clickless is None):
-#          raise PreventUpdate
-#     else:
-#         print(clickless, clickmore)
-#     return None
+        return checklist + [new_checklist(len(checklist) + 1, init_val=init_val)]
 
 
-def new_checklist(i, init_val=None):
-        return dcc.Checklist(
-        options=get_categories(),
-        value=init_val,
-        labelStyle={"display": "inline-block"},
-        id="checklist-{}".format(i)
+
+@app.callback(
+    Output("patientid-disp", "children"),
+    Output("latest-update-patsearch", "children"),
+    Input("patsearch-button", "n_clicks"),
+    Input("input-patid", "value"),
 )
-
-
-app.layout = html.Div([navbar,
-                 html.Div(id='body-div', style={"margin-top": "0px"}),
-                 html.Div([
-                     html.Button('More', id='morechecklist-button'),
-                     html.Button('Less', id='lesschecklist-button'),
-                     html.Button('Update', id='updatechecklists-button')]),
-                    html.Div(id="div-checklists", children=[]),
-                html.P("This here", id="checklist-test")
-                 ])
+def cb_render(n_clicks, patid):
+    if n_clicks is None:
+        raise PreventUpdate
+    else:
+        start_=datetime.now()
+        d = {k: execquerey(pat_data_q(k, patid, col="ids__uid"), engine) for k in all_data_tables}
+        return [html.P(", ".join([k for k, v in d.items() if v.shape[0] > 0]))], get_update_status(start_)
 
 
 @app.callback(
@@ -188,11 +179,11 @@ def update_checklist_test(n_clicks, checklists):
     if n_clicks is None:
         raise PreventUpdate
     else:
-        OUT =[]
+        OUT = []
         for v in checklists:
-            #print("Updating the text..", v)
             if len(v["props"]["value"]) > 0:
-                thequery = "select ids__uid from view__uid_has where {}".format(cond_from_checklist(v["props"]["value"]))
+                thequery = "select ids__uid from view__uid_has where {}".format(
+                    cond_from_checklist(v["props"]["value"]))
 
                 with engine.connect() as con:
                     dout = pd.read_sql(thequery, con).values.reshape(-1)
@@ -200,5 +191,33 @@ def update_checklist_test(n_clicks, checklists):
         return "\n".join(OUT)
 
 
+
+
+app.layout = html.Div([
+    separator,
+    navbar,
+    separator,
+    html.H2("Population study", style={'text-align': 'left'}),
+    html.Div(id='body-div', style={"margin-top": "0px"}),
+    html.Div([
+        html.Button('More', id='morechecklist-button'),
+        html.Button('Less', id='lesschecklist-button'),
+        html.Button('Update', id='updatechecklists-button')
+    ]),
+    html.Div([
+        html.Div(id="div-checklists", children=[]),
+        html.P("This here", id="checklist-test")
+    ]),
+    separator,
+    html.H2("Patient Display", style={'text-align': 'left'}),
+    html.Div([
+        dcc.Input(id="input-patid", placeholder="Write Patient ID"),
+        html.Button('Search', id='patsearch-button'),
+        get_latest_update(id="latest-update-patsearch"),
+        html.Div(id="patientid-disp")
+    ]),
+    separator
+])
+
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True)

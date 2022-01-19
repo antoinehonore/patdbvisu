b5e866d99c8381e39744552571106df9fa0b9276
Antoine Honoré <honore@kth.se>
2022-01-19 18:41:45 +0100

    minor

--- bin/views2.py
+++ bin/views2.py
@@ -1,8 +1,8 @@
 import argparse
-from utils import get_engine, get_dbcfg, read_query_file, clin_tables, all_data_tables, run_select_queries, get_size_interval, pidprint
+from src.utils import get_engine, get_dbcfg, read_query_file, clin_tables, all_data_tables, run_select_queries, get_size_interval, pidprint
 import pandas as pd
 import sys
-from utils import get_colnames
+from src.utils import get_colnames, get_size_patient, get_size_interval, define_col_db_size_query, get_colnames
 
 dbcfg = get_dbcfg("cfg/db.cfg")
 
@@ -18,12 +18,13 @@ import plotly.express as px
 
 import numpy as np
 
-def get_med_str(dd,digits=3)-> list:
+
+def get_med_str(dd, digits=3) -> list:
     """Compute medians for each of the scores(i.e. columns) in the input `pd.DataFrame`."""
     return [str(x) for x in np.round(dd.median(), digits).values]
 
 
-def get_q_str(dd,digits=3,short=False)->list:
+def get_q_str(dd, digits=3, short=False) -> list:
     """Format the inter-quartile range for each score."""
     tmp_quartile = tuple(np.round(dd.quantile([0.25, 0.75]).values, digits))
     Q1, Q3 = tuple(tmp_quartile)
@@ -45,72 +46,23 @@ def format_desc(dd, dtype=np.float64, digits=3, short=False):
 
 all_data_tables.pop(0)
 
+from multiprocessing import Pool
+from functools import partial
+from datetime import datetime
+
+
+def g(x, con=None):
+    return pd.DataFrame.from_dict(get_size_interval(x, con), orient="index").T
+
 
 if __name__ == "__main__":
     args = parser.parse_args()
     thepatid = "c656cb9d57076c8b57614e4141dd90f4d36580c0ae47aeda86098def5701d7dc"
     l = []
     with engine.connect() as con:
-        df = pd.read_sql("select ids__uid, ids__interval from monitorhf where ids__uid='{}'".format(thepatid), con)
-        for i, s in enumerate(df["ids__interval"].values):
-            l.append(get_size_interval(s, con))
-            pidprint(i, l[-1])
+        ilist = get_pat_intervals(thepatid, con)
 
-    print("")
-    transpose_q = \
-    "  SELECT 'SELECT * FROM   crosstab(       $ct$SELECT u.attnum, t.rn, u.val         FROM  (SELECT row_number() OVER () AS rn, * FROM ' || attrelid::regclass || ') t "\
-      "       , unnest(ARRAY[' || string_agg(quote_ident(attname) "\
-     "                         || '::text', ',') || ']) "\
-     "            WITH ORDINALITY u(val, attnum) "\
-    "    ORDER  BY 1, 2$ct$ "\
-   ") t (attnum bigint, ' "\
-     "|| (SELECT string_agg('r'|| rn ||' text', ', ') "\
-      "   FROM  (SELECT row_number() OVER () AS rn FROM tbl) t) "\
-     "|| ')' AS sql "\
-"FROM   pg_attribute "\
-"WHERE  attrelid = 'tbl'::regclass "\
-"AND    attnum > 0 AND    NOT attisdropped " \
-"GROUP  BY attrelid;"
-
-
-
-
-def get_colnames(k,con):
-    df=pd.read_sql("select column_name   FROM information_schema.columns "\
-                   " WHERE table_schema = \'public\'    AND table_name   = \'{}\' ".format(k),
-                   con)
-    return list(map(lambda s: "\""+s+"\"", df["column_name"].values.tolist()))
-with engine.connect() as con:
-    all_cols={k:get_colnames(k, con) for k in all_data_tables}
-
-ref_cols = ["\""+s+"\"" for s in ['ids__uid', 'ids__interval', 'interval__raw', 'interval__start', 'interval__end']]
-
-thecase = "case when ({} notnull) then True else NULL end as {}"
-
-the_cases = {k:",\n".join([col if (k == "overview") or (col in ref_cols) else thecase.format(col, col)
-                           for col in all_cols[k]]) for k in all_data_tables}
-
-
-patid = "f44e1b73551b133630599a55037768ce87ead7e64d37ce1f3e906a6e21a7a5b1"
-
-
-create_views_queries = {k: "create view patview_{}_{} as (select * from {} where ids__uid='{}');".format(k, patid, k, patid) for k in all_data_tables}
-
-engine.execute("\n".join([v for v in create_views_queries.values()]))
-
-
-select_queries = {k: "select {} from patview_{}_{}".format(the_cases[k], k, patid) for k in all_data_tables}
-
-
-data_lvl1 = run_select_queries(select_queries, engine)
-
-drop_views_queries = {k: "drop view if exists patview_{}_{};".format(k, patid) for k in all_data_tables}
-
-
-
-
-
-engine.execute("\n".join([v for v in drop_views_queries.values()]))
-
-
-
+        f = partial(g, con=con)
+        start_ = datetime.now()
+        out = list(map(f, ilist))
+        print((datetime.now() - start_).total_seconds()/len(out), "sec/interv")
